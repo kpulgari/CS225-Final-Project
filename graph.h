43273cc9d45de8fe6dac0506474dcdca74847d1b
Kaushik Pulgari <kpulg2@illinois.edu>
2022-11-10 21:34:51 -0600

    Create graph data structure

---
+++ graph.h
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <map>
+
+class Graph {
+    public:
+        struct Node {
+            std::string title;
+            std::vector<int> edges;
+        };
+
+        std::map<int, Node*> map; 
+};
